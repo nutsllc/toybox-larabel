10e9b74190dc4ac3e049a7b19c60b478c0722fab
@ontheroad_jp <dev@ontheroad.jp>
2016-11-01 22:17:40 +0900

    [Fix] typo

--- 5.2/php5.6-apache/docker-entrypoint.sh
+++ 5.2/php5.6-apache/docker-entrypoint.sh
@@ -16,17 +16,27 @@
 
     cd /var/www
     tar xzf ${HOME}/laravel.tar.gz && {
-        chown -R www-data:www-data /var/www/laravel
         env=/var/www/laravel/.env
         config_app=/var/www/laravel/config/app.php
         sed -i -e "s/^\(DB_CONNECTION=\).*$/\1${DB_CONNECTION}/" ${env}
-        sed -i -e "s/^\(DB_HOST=\).*$/\1${DB_HOST}/" ${env}
-        sed -i -e "s/^\(DB_PORT=\).*$/\1${DB_PORT}/" ${env}
-        sed -i -e "s/^\(DB_DATABASE=\).*$/\1${DB_DATABASE}/" ${env}
-        sed -i -e "s/^\(DB_USERNAME=\).*$/\1${DB_USERNAME}/" ${env}
-        sed -i -e "s/^\(DB_PASSWORD=\).*$/\1${DB_PASSWORD}/" ${env}
         sed -i -e "s:^\(.*'timezone' => \)'UTC',$:\1'${LARAVEL_TZ}',:" ${config_app}
         sed -i -e "s/^\(.*'locale' => \)'en',$/\1'${LARAVEL_LOCALE}',/" ${config_app}
+
+        if [ "${DB_CONNECTION}" = "sqlite" ]; then
+            sed -i -e "s/^\(DB_HOST=.*\)/#\1/" ${env}
+            sed -i -e "s/^\(DB_PORT=.*\)/#\1/" ${env}
+            sed -i -e "s/^\(DB_DATABASE=.*\)/#\1/" ${env}
+            sed -i -e "s/^\(DB_USERNAME=.*\)/#\1/" ${env}
+            sed -i -e "s/^\(DB_PASSWORD=.*\)/#\1/" ${env}
+            touch /var/www/laravel/database/database.sqlite
+        else
+            sed -i -e "s/^\(DB_HOST=\).*$/\1${DB_HOST}/" ${env}
+            sed -i -e "s/^\(DB_PORT=\).*$/\1${DB_PORT}/" ${env}
+            sed -i -e "s/^\(DB_DATABASE=\).*$/\1${DB_DATABASE}/" ${env}
+            sed -i -e "s/^\(DB_USERNAME=\).*$/\1${DB_USERNAME}/" ${env}
+            sed -i -e "s/^\(DB_PASSWORD=\).*$/\1${DB_PASSWORD}/" ${env}
+        fi
+        chown -R www-data:www-data /var/www/laravel
         php /var/www/laravel/artisan migrate
         rm ${HOME}/laravel.tar.gz
     }
