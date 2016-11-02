c4d4ca5e9d70bdf6d7726f64c8dbfb326a485d4e
@ontheroad_jp <dev@ontheroad.jp>
2016-11-02 22:11:26 +0900

    [New] adding data-container

--- bin/init.sh
+++ bin/init.sh
@@ -13,6 +13,12 @@ dirs=(
     "${dist}/5.3/php7.0-apache"
 )
 
+dirs_data=(
+    "${dist}/5.1/data-container"
+    "${dist}/5.2/data-container"
+    "${dist}/5.3/data-container"
+)
+
 for d in ${dirs[@]}; do
     [ -d ${d} ] && rm -r ${d}
     lara_ver=$(echo ${d} | awk -F "/" '{print $(NF - 1)}').*
@@ -26,6 +32,19 @@ for d in ${dirs[@]}; do
     sed -i -e "s/{{PHP_VERSION}}/${php_ver}/" ${d}/Dockerfile
     echo "done."
 done
+
+for d in ${dirs_data[@]}; do
+    [ -d ${d} ] && rm -r ${d}
+    lara_ver=$(echo ${d} | awk -F "/" '{print $(NF - 1)}').*
+    printf "Generate: Dockerfile for Laravel ${lara_ver} data container ..."
+    mkdir -p ${d}
+    cp ${src}/data-container/Dockerfile-seed ${d}/Dockerfile
+    cp ${src}/data-container/docker-entrypoint.sh-seed ${d}/docker-entrypoint.sh
+    chmod 755 ${d}/docker-entrypoint.sh
+    sed -i -e "s/{{LARAVEL_VERSION}}/${lara_ver}/" ${d}/Dockerfile
+    echo "done."
+done
+
 echo "complete!"
 
 exit 0
