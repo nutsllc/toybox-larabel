36850270607026bb2e4a3246e1027614da263ec7
@ontheroad_jp <dev@ontheroad.jp>
2016-11-23 13:44:23 +0900

    [Modify] all of it

--- 5.2/php7.0-apache/docker-entrypoint.sh
+++ 5.2/php7.0-apache/docker-entrypoint.sh
@@ -1,49 +1,59 @@
 #!/bin/bash
 
-: ${DB_CONNECTION:=mysql}
-: ${DB_HOST:=mariadb}
-: ${DB_PORT:=3306}
-: ${DB_DATABASE:=laradb}
-: ${DB_USERNAME:=lara}
-: ${DB_PASSWORD:=password}
+: ${DOCUMENT_ROOT:=/var/www/laravel/public}
+${LARAVEL_ROOT}=${DOCUMENT_ROOT}/../
 : ${LARAVEL_TZ:=UTC}
 : ${LARAVEL_LOCALE:=en}
 
-[ -f ${HOME}/laravel.tar.gz ] && {
-    site_confdir=/etc/apache2/sites-available
-    sed -i -e "s:^\(.*DocumentRoot \)/var/www/html$:\1${DOCUMENT_ROOT}:" ${site_confdir}/000-default.conf
-    sed -i -e "s:^\(.*DocumentRoot \)/var/www/html$:\1${DOCUMENT_ROOT}:" ${site_confdir}/default-ssl.conf
+# Apache settings
+site_confdir=/etc/apache2/sites-available
+sed -i -e "s:^\(.*DocumentRoot \)/var/www/html$:\1${DOCUMENT_ROOT}:" ${site_confdir}/000-default.conf
+sed -i -e "s:^\(.*DocumentRoot \)/var/www/html$:\1${DOCUMENT_ROOT}:" ${site_confdir}/default-ssl.conf
 
-    [ ! -f /var/www/laravel/.env ] && {
-        cd /var/www
-        tar xzf ${HOME}/laravel.tar.gz 
-        env=/var/www/laravel/.env
-        config_app=/var/www/laravel/config/app.php
-        sed -i -e "s/^\(DB_CONNECTION=\).*$/\1${DB_CONNECTION}/" ${env}
-        sed -i -e "s:^\(.*'timezone' => \)'UTC',$:\1'${LARAVEL_TZ}',:" ${config_app}
-        sed -i -e "s/^\(.*'locale' => \)'en',$/\1'${LARAVEL_LOCALE}',/" ${config_app}
-    
+[ -f ${HOME}/laravel.tar.gz ] && {
+    cd ${LARAVEL_ROOT}/../
+    tar xzf ${HOME}/laravel.tar.gz 
+    rm ${HOME}/laravel.tar.gz
+}
+
+config_app=${LARAVEL_ROOT}/config/app.php
+sed -i -e "s:^\(.*'timezone' => \)'UTC',$:\1'${LARAVEL_TZ}',:" ${config_app}
+sed -i -e "s/^\(.*'locale' => \)'en',$/\1'${LARAVEL_LOCALE}',/" ${config_app}
+
+env=${LARAVEL_ROOT}/.env
+[ -f ${env} ] && {
+
+    # DB settings
+    [ -n ${DB_CONNECTION} ] && sed -i -e "s/^\(DB_CONNECTION=\)mysql$/\1${DB_CONNECTION}/" ${env} && {
         if [ "${DB_CONNECTION}" = "sqlite" ]; then
             sed -i -e "s/^\(DB_HOST=.*\)/#\1/" ${env}
             sed -i -e "s/^\(DB_PORT=.*\)/#\1/" ${env}
-            sed -i -e "s/^\(DB_DATABASE=.*\)/#\1/" ${env}
             sed -i -e "s/^\(DB_USERNAME=.*\)/#\1/" ${env}
             sed -i -e "s/^\(DB_PASSWORD=.*\)/#\1/" ${env}
-            touch /var/www/laravel/database/database.sqlite
+            if [ -z "${DB_DATABASE}" ]; then
+                sed -i -e "s/^\(DB_DATABASE=.*\)/#\1/" ${env}
+            elif [ ! -f ${LARAVEL_BASE}/database/${DB_DATABASE} ]; then
+                touch ${LARAVEL_ROOT}/database/${DB_DATABASE}
+            fi
         else
-            sed -i -e "s/^\(DB_HOST=\).*$/\1${DB_HOST:=mariadb}/" ${env}
-            sed -i -e "s/^\(DB_PORT=\).*$/\1${DB_PORT:=3306}/" ${env}
-            sed -i -e "s/^\(DB_DATABASE=\).*$/\1${DB_DATABASE:=laradb}/" ${env}
-            sed -i -e "s/^\(DB_USERNAME=\).*$/\1${DB_USERNAME:=lara}/" ${env}
-            sed -i -e "s/^\(DB_PASSWORD=\).*$/\1${DB_PASSWORD:=password}/" ${env}
+            [ -n "${DB_HOST}" ] && sed -i -e "s/^\(DB_HOST=\).*$/\1${DB_HOST}/" ${env}
+            [ -n "${DB_PORT}" ] && sed -i -e "s/^\(DB_PORT=\).*$/\1${DB_PORT}/" ${env}
+            [ -n "${DB_DATABASE}" ] && sed -i -e "s/^\(DB_DATABASE=\).*$/\1${DB_DATABASE}/" ${env}
+            [ -n "${DB_USER_NAME}" ] && sed -i -e "s/^\(DB_USERNAME=\).*$/\1${DB_USERNAME}/" ${env}
+            [ -n "${DB_PASSWORD}" ] && sed -i -e "s/^\(DB_PASSWORD=\).*$/\1${DB_PASSWORD}/" ${env}
         fi
-        chown -R www-data:www-data /var/www/laravel
-        php /var/www/laravel/artisan migrate
     }
 
-    [ -f ${HOME}/laravel.tar.gz ] && {
-        rm ${HOME}/laravel.tar.gz
-    }
+    # Mail settings
+    [ -n "${MAIL_DRIVER}" ] && sed -i -e "s/^\(MAIL_DRIVER=\)smtp$/\1${MAIL_DRIVER}/" ${env}
+    [ -n "${MAIL_HOST}" ] && sed -i -e "s/^\(MAIL_HOST=\)mailtrap.io$/\1${MAIL_DRIVER}/" ${env}
+    [ -n "${MAIL_PORT}" ] && sed -i -e "s/^\(MAIL_PORT=\)2525$/\1${MAIL_DRIVER}/" ${env}
+    [ -n "${MAIL_USERNAME}" ] && sed -i -e "s/^\(MAIL_USERNAME=\)null$/\1${MAIL_DRIVER}/" ${env}
+    [ -n "${MAIL_PASSWORD}" ] && sed -i -e "s/^\(MAIL_PASSWORD=\)null$/\1${MAIL_DRIVER}/" ${env}
+    [ -n "${MAIL_ENCRYPTION}" ] && sed -i -e "s/^\(MAIL_ENCRYPTION=\)null$/\1${MAIL_DRIVER}/" ${env}
 }
+
+chown -R www-data:www-data ${LARAVEL_ROOT}
+#php ${DOCUMENT_ROOT}/../artisan migrate
 
 exec $@
